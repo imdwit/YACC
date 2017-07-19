8c0753ece4a3c5144c2b397081820d32666f797f
Dwight Doane <dwightdoane@me.com>
2017-07-19 13:45:45 -0400

    adds test for progress's computed prop

--- test/components/mainApp.spec.js
+++ test/components/mainApp.spec.js
@@ -18,10 +18,10 @@ describe('MainApp', () => {
     expect($ctrl.tasks.length).toBe(6);
   });
 
-  it('should toggle a task\'s completed prop', () => {
+  it("should toggle a task's completed prop", () => {
     const $ctrl = $componentController('mainApp', null, null);
     $ctrl.$onInit();
-    
+
     expect($ctrl.toggleComplete).toBeDefined();
 
     const $index = 3;
@@ -29,5 +29,19 @@ describe('MainApp', () => {
     expect($ctrl.tasks[$index].completed).toBe(true);
     $ctrl.toggleComplete($index);
     expect($ctrl.tasks[$index].completed).toBe(false);
+  });
+
+  it('should calculate progress of completed tasks', () => {
+    const locals = {};
+    const $ctrl = $componentController('mainApp', locals, null);
+    $ctrl.$onInit();
+    expect($ctrl.progress).toBe(0);
+
+    const $index = 3;
+    locals.$scope.$digest();
+    $ctrl.toggleComplete($index);
+    locals.$scope.$digest();
+    const progress = 1 / $ctrl.tasks.length;
+    expect($ctrl.progress).toBe(progress);
   });
 });
