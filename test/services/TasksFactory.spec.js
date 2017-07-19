fc3b57bb872641cd174d4d96deb73851b8b550bc
Dwight Doane <dwightdoane@me.com>
2017-07-19 10:23:56 -0400

    adds test cases for the TasksService

--- test/services/TasksFactory.spec.js
+++ test/services/TasksFactory.spec.js
@@ -1,8 +1,28 @@
 import TasksService from '@/services/Tasks.factory';
 
 describe('TasksService', () => {
-  it('should hello world', () => {
-    const x = TasksService();
-    expect(true).toBe(true);
+  it('should return all tasks', () => {
+    const tasksService = TasksService();
+    expect(tasksService.fetchTasks).toBeDefined();
+
+    const tasks = tasksService.fetchTasks();
+    expect(tasks.length).toBe(10);
+  });
+
+  it('should return 6 tasks', () => {
+    const tasksService = TasksService();
+    expect(tasksService.fetchRandomTasks).toBeDefined();
+
+    const tasks = tasksService.fetchRandomTasks();
+    expect(tasks.length).toBe(6);
+  });
+
+  it('should return 6 random tasks', () => {
+    const tasksService = TasksService();
+    expect(tasksService.fetchRandomTasks).toBeDefined();
+
+    const firstSet = tasksService.fetchRandomTasks();
+    const secondSet = tasksService.fetchRandomTasks();
+    expect(firstSet).not.toEqual(secondSet);
   });
 });
