318583d2ac1ffb0ba79d8c35bb24600aeba970f3
Dwight Doane <dwightdoane@me.com>
2017-07-19 13:43:54 -0400

    imports the directives module

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+import jQuery from 'jquery';
 import angular from 'angular';
 import services from './services/module';
 import components from './components/module';
+import directives from './directives/module';
 
 const requires = [
   services.name,
   components.name,
+  directives.name,
 ];
 
 const app = angular.module('app', requires);
