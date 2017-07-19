0890b89787226734193e2306fa0d3a67ae8d6c0d
Dwight Doane <dwightdoane@me.com>
2017-07-19 12:07:10 -0400

    renders the list of tasks and wires up click handlers. as well as toggles some classes

--- src/components/mainApp.component.js
+++ src/components/mainApp.component.js
@@ -1,5 +1,28 @@
+import './mainApp.css';
+
+const template = `
+  <div class="main">
+    <p class="main__title">Tasks for today:</p>
+    <div class="tasks">
+      <div
+        class="tasks__task"
+        ng-repeat="task in $ctrl.tasks track by $index"
+        >
+          <p
+            class="task__task"
+            ng-class="task.completed ? 'task__task--completed': ''"
+            ng-click="$ctrl.toggleComplete($index)"
+            >
+              {{task.task}}
+          </p>
+      </div>
+    </div>
+  </div>
+`;
+
 const mainApp = {
   controller: MainAppCtrl,
+  template,
 };
 
 function MainAppCtrl(TasksService) {
@@ -13,5 +36,6 @@ function MainAppCtrl(TasksService) {
 }
 
 MainAppCtrl.$inject = ['TasksService'];
+
 
 export default mainApp;
