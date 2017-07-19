5c08901143eeac2af1a37d4e3125b2f1afa7e8b0
Dwight Doane <dwightdoane@me.com>
2017-07-19 12:06:27 -0400

    imports the services and components modules and registers them with app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import angular from 'angular';
+import services from './services/module';
+import components from './components/module';
 
 const requires = [
-
+  services.name,
+  components.name,
 ];
 
 const app = angular.module('app', requires);
